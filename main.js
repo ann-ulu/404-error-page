a2b7e48dcbe2d69a63c9b14f62fbcab4477d1862
ann-ulu <uluann1@gmail.com>
2023-06-27 20:50:05 +0100

    observer

---
+++ main.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const picElement = document.querySelectorAll('.pic');
+picElement.forEach((el) => observer.observe(el));
